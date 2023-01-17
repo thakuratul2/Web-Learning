f6cbe1db655f8799d3d04e74baa43a7d3b84a5de
thakuratul2 <pratapsinghatul111@gmail.com>
2023-01-17 15:48:14 +0530

    Datatypes

---
+++ php/datatypes.php
@@ -0,0 +1,25 @@
+<?php
+//datatypes
+/*
+1.Stings
+2.Integer
+3.Array
+4.Boolean
+5.Float
+6.Object
+*/
+
+$name = "Atul Pratap Singh";
+$num = 15;
+$txt = array("BMW", "Audi");
+$t = true;
+$fl = 1.25;
+
+echo "$name<br>";
+echo "$num<br>";
+var_dump($txt);
+echo "$t<br>";
+echo "$fl<br>";
+
+
+?>
