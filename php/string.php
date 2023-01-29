e2db35d0c246507675418a2cfbc9a6ca7212309a
thakuratul2 <pratapsinghatul111@gmail.com>
2023-01-29 11:21:37 +0530

    String

---
+++ php/string.php
@@ -0,0 +1,19 @@
+<?php
+/*
+1. strlen()
+2. strrev()
+3. str_word_count()
+4. strpos()
+5. str_place()
+*/
+$name = "Hello World";
+
+print_r(strlen($name));
+echo "<pre>";
+print_r(strrev($name));
+echo "<pre>";
+print_r(str_shuffle($name));
+
+
+
+?>
